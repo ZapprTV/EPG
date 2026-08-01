b506c18f7c163c1e1268e58ca05279d78fe8442a
Francesco <52544979+FrancescoRosi@users.noreply.github.com>
2026-08-01 15:34:33 +0200

    aggiunte Canal+ (en clair), France 24 e TV5Monde in Francia

--- lists/fr/tnt/national.js
+++ lists/fr/tnt/national.js
@@ -4,7 +4,7 @@ import samsungtvplus from "../../../sources/samsungtvplus";
 import plutotv from "../../../sources/plutotv";
 
 export default {
-    blue: { fetch: blue, channels: [601, 182, 184, 185, 186, 249, 28, 653, 609, 295, 195, 43, 217, 241, 1064, 115, 2166, 2182, 199, 245, 9, 298, 353, 93] },
+    blue: { fetch: blue, channels: [601, 182, 184, 185, 186, 249, 28, 653, 609, 295, 195, 43, 217, 241, 1064, 115, 2166, 2182, 199, 245, 9, 298, 353, 93, 63, 183, 1639, 635] },
     sfr: { fetch: sfr, channels: [234] },
     samsungtvplus: { fetch: samsungtvplus, channels: ["fr"] },
     plutotv: { fetch: plutotv, channels: ["fr"] }
